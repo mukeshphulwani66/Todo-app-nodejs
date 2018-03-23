57cb694d87b9a06b82ba4b5bed71256f5963f771
mukesh <mukeshphulwani55@gmail.com>
2018-03-23 20:02:19 +0530

    first app one

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var todoController = require('./controllers/todoController');
-var port = 5000 || process.env.PORT;
+var port = process.env.PORT || 5000;
 app.set('view engine','ejs');
 
 app.use(express.static('./public'));
@@ -9,5 +9,6 @@ app.use(express.static('./public'));
 //fire controller
 todoController(app);
 
-app.listen(port);
-console.log('listening');
+app.listen(port ,function(){
+    console.log('listening' + port); 
+});
